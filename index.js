1ecd84ca82c7a255a2a9a5f8a8b572ea1e1fff78
Cole Kendall <cole.kendall.18@cnu.edu>
2021-06-27 00:17:51 -0400

    Add tenative plan inside index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+/*
+Plan: Webchat using mean stack
+
+1) Create a node.js server that, utilizing express.js, supplies connected
+    clients with a chat terminal.
+    - Probibly will be best if I used Jade (More stackexchange support)
+    - Probibly will be best if I used Bootstrap (Because vanilla CSS hurts and
+        I dont know sass or that other hip CSS interperetor)
+    - Remeber, use mongoDB, i swear if I accidentally use MySQL out of reflex i am gonna have a cow!
+
+2) Before I can be confident with the node.js server code I will need to research mongoDB and Angular...
+    - Before even starting this project I will need to read up on Angular
+    - mongoDB is hopefully easier to learn than Angular
+
+3) Componets I will need to write:
+    - server.js (initialises the node server)
+    - index.jade (the html the intergrates with node better than actual HTML)
+    - something with angular... Probibly...
+
+4) Step by step plan (Tenative... OOOOH BIG WORDSS)
+    -1  Reveiw "a few" videos about mongoDB and Angular
+    -2  Program an effective development enviorment for testing
+        ~ Make sure I can connect to the server via localhost
+        ~ Consider Test Driven development
+    -3  Begin writing the server.js
+        ~ Attempt to avoid any mongoDB and angular until all
+            technoligies I am comfortable with are working.
+            This will allow me to isolate errors related to misuse
+            unfamiliar technoligies.
+    -4  Write index.Jade
+
+    Hopfully this is all I need to do...
+*/
